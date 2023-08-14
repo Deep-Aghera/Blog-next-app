91b7b2e492116e282e8d0561945c168034d579a6
Deep <deep.aghera@marutitech.com>
2023-08-14 18:48:24 +0530

    footer change

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const Footer = () => {
   return (
     <div className="flex flex-row bg-neutral-800  p-4 text-white">
-      <div className="w-2/4 md:w-2/3">
+      <div className="w-2/4 md:w-2/3">  
         <Link href="/">
           <span className="text-lg md:text-xl font-semibold">
            About Us
