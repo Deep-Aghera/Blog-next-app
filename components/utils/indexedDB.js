e0b60ef742f712d2d0c944e9253fe41c3deb9147
Deep <deep.aghera@marutitech.com>
2023-08-17 11:22:44 +0530

    dev merged

--- components/utils/indexedDB.js
+++ components/utils/indexedDB.js
@@ -33,10 +33,26 @@ export async function fetchPostById(db, id) {
 }
 
 export async function updateReadStatus(db, id, inRead) {
+  
   const tx = db.transaction('readingList', 'readwrite');
   const store = tx.objectStore('readingList');
 
   await store.put({ id, inRead });
 
   return inRead;
+}
+
+async function countInReads() {
+  let count = 0
+   const db = await initIndexedDB();
+   const tx = db.transaction('readingList', 'readonly');
+   const store = tx.objectStore('readingList');
+   const data = await store.getAll()
+   data.map((item) => {
+    if(item.inRead === true) {
+      count++
+    }
+   })
+   console.log(data,count);
+   return count
 }
